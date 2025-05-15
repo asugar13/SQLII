de8cfbc779fc664cb604fd2ece192f45a2cef8a5
luisguareschi <luisguareschi@hotmail.com>
2025-05-15 16:10:26 -0400

    Added a command to drop the existing schema `mydb` before creating it in ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -12,6 +12,8 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `mydb`;
+
 CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 USE `mydb` ;
 
